76625f19ec15ca0c2084878cf012a5f47e406829
Patrick Delaney <delaney.patrick17@gmail.com>
2018-12-23 16:59:01 -0500

    referenced more sample content for the contact and about pages

--- MyCoreWebApp/Controllers/HomeController.cs
+++ MyCoreWebApp/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace MyCoreWebApp.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "The services we provide can go here";
 
             return View();
         }
